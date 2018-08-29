e193a705005fd0edb2173e9400539a2be2dbc624
Damien Frikha <damien.frikha@kleegroup.com>
2018-08-29 19:47:30 +0200

    Export excel basique.

--- Kinetix.Reporting/Internal/Excel/WorksheetBuilder.cs
+++ Kinetix.Reporting/Internal/Excel/WorksheetBuilder.cs
@@ -7,6 +7,10 @@ namespace Kinetix.Reporting.Internal.Excel
 {
     internal class WorksheetBuilder<T> : IWorksheetBuilder<T>
     {
+        private readonly IList<(string label, Func<T, object> selector)> _columns = new List<(string label, Func<T, object> selector)>();
+        private IEnumerable<T> _data;
+        private bool _transpose = false;
+
         /// <inheritdoc />
         public IXLWorksheet Worksheet { get; set; }
 
@@ -16,7 +20,34 @@ namespace Kinetix.Reporting.Internal.Excel
         /// <inheritdoc cref="IWorksheetBuilder{T}.Build" />
         public IExcelBuilder Build()
         {
-            // TODO
+            for (var i = 0; i < _columns.Count; i++)
+            {
+                var (label, selector) = _columns[i];
+                if (_transpose)
+                {
+                    Worksheet.Cell(i + 1, 1).Value = label;
+                }
+                else
+                {
+                    Worksheet.Cell(1, i + 1).Value = label;
+                }
+
+                var j = 1;
+                foreach (var item in _data)
+                {
+                    j++;
+                    if (_transpose)
+                    {
+                        Worksheet.Cell(i + 1, j).SetValue(selector(item));
+                    }
+                    else
+                    {
+                        Worksheet.Cell(j, i + 1).SetValue(selector(item));
+                    }
+                }
+            }
+
+            Worksheet.ColumnsUsed().AdjustToContents();
             return ExcelBuilder;
         }
 
@@ -29,21 +60,21 @@ namespace Kinetix.Reporting.Internal.Excel
         /// <inheritdoc cref="IWorksheetBuilder{T}.Column(string, Func{T, object})" />
         public IWorksheetBuilder<T> Column(string label, Func<T, object> selector)
         {
-            // TODO
+            _columns.Add((label, selector));
             return this;
         }
 
         /// <inheritdoc cref="IWorksheetBuilder{T}.Data" />
         public IWorksheetBuilder<T> Data(IEnumerable<T> data)
         {
-            // TODO
+            _data = data;
             return this;
         }
 
         /// <inheritdoc cref="IWorksheetBuilder{T}.Transpose" />
         public IWorksheetBuilder<T> Transpose()
         {
-            // TODO
+            _transpose = true;
             return this;
         }
     }
