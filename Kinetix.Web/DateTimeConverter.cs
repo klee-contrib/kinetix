4ce39b9a9941c3dc206581a2eca420c9bc7a5b48
Damien Frikha <damien.frikha@kleegroup.com>
2021-06-02 10:04:46 +0200

    Je pense que ça va marcher la pour les dates

--- Kinetix.Web/DateTimeConverter.cs
+++ Kinetix.Web/DateTimeConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,12 +9,12 @@ namespace Kinetix.Web
     {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return DateTime.Parse(reader.GetString());
+            return DateTime.Parse(reader.GetString(), CultureInfo.InvariantCulture);
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ssZ"));
+            writer.WriteStringValue(value.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ssZ"));
         }
     }
 }
