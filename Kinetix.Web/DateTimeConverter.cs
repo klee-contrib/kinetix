612124dd0092d3503575407418f507e599e7e8b1
Damien Frikha <damien.frikha@kleegroup.com>
2021-06-02 10:20:12 +0200

    Essayons encore

--- Kinetix.Web/DateTimeConverter.cs
+++ Kinetix.Web/DateTimeConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -9,7 +8,7 @@ namespace Kinetix.Web
     {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return DateTime.Parse(reader.GetString(), CultureInfo.InvariantCulture);
+            return DateTimeOffset.Parse(reader.GetString()).DateTime;
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
