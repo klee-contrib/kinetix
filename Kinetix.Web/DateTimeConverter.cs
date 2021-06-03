ba98fb67e22daa635561a2c8ee78991928a356c8
Damien Frikha <damien.frikha@kleegroup.com>
2021-06-03 14:49:05 +0200

    Fix serialization TimeSpan

--- Kinetix.Web/DateTimeConverter.cs
+++ Kinetix.Web/DateTimeConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -6,14 +7,31 @@ namespace Kinetix.Web
 {
     public class DateTimeConverter : JsonConverter<DateTime>
     {
+        /// <inheritdoc />
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             return DateTimeOffset.Parse(reader.GetString()).DateTime;
         }
 
+        /// <inheritdoc />
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
             writer.WriteStringValue(value.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ssZ"));
+        }
+    }
+
+    public class TimeSpanConverter : JsonConverter<TimeSpan>
+    {
+        /// <inheritdoc />
+        public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return TimeSpan.ParseExact(reader.GetString(), "c", CultureInfo.InvariantCulture);
+        }
+
+        /// <inheritdoc />
+        public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("c", CultureInfo.InvariantCulture));
         }
     }
 }
